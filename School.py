2d2a391f3951d64e13440f033ff785fc41837f07
Gsaipavan08 <146938885+Gsaipavan08@users.noreply.github.com>
2023-11-01 12:23:34 +0530

    School.py

--- School.py
+++ School.py
@@ -7,16 +7,16 @@ class School:
         self.subject = subject
 
 
-class1 = School("Math A", "Mr. Smith", ["Alice", "Bob", "Charlie"], 101, "Mathematics")
-class2 = School("English B", "Ms. Johnson", ["David", "Emma", "Frank"], 102, "English")
-class3 = School("Science C", "Mr. Wilson", ["Grace", "Henry", "Ivy"], 103, "Science")
-class4 = School("History D", "Mrs. Davis", ["Jack", "Kate", "Liam"], 104, "History")
-class5 = School("Art E", "Mr. Brown", ["Olivia", "Peter", "Quinn"], 105, "Art")
-class6 = School("Music F", "Ms. Lee", ["Rachel", "Sam", "Tom"], 106, "Music")
-class7 = School("Physical Education G", "Coach Smith", ["Sophia", "Tyler", "Victoria"], 107, "Physical Education")
-class8 = School("Computer Science H", "Mr. Johnson", ["William", "Xavier", "Yasmine"], 108, "Computer Science")
-class9 = School("Geography I", "Mrs. Adams", ["Zoe", "Daniel", "Ella"], 109, "Geography")
-class10 = School("Foreign Language J", "Ms. Garcia", ["Gabriel", "Hannah", "Isaac"], 110, "Foreign Language")
+class1 = School("Math A", "Ram", ["Alice", "Bob", "Charlie"], 101, "Mathematics")
+class2 = School("English B", "Raja", ["David", "Emma", "Frank"], 102, "English")
+class3 = School("Biology A", "Pavan", ["Grace", "Henry", "Ivy"], 103, "Science")
+class4 = School("Physics A", "Kiran", ["Jack", "Kate", "Liam"], 104, "History")
+class5 = School("English A", "Mr. Brown", ["Olivia", "Peter", "Quinn"], 105, "Art")
+class6 = School("Maths A", "Ms. Lee", ["Rachel", "Sam", "Tom"], 106, "Music")
+class7 = School("Biology B", "Coach Smith", ["Sophia", "Tyler", "Victoria"], 107, "Physical Education")
+class8 = School("Social A", "Mr. Johnson", ["William", "Xavier", "Yasmine"], 108, "Computer Science")
+class9 = School("Social B", "Mrs. Adams", ["Zoe", "Daniel", "Ella"], 109, "Geography")
+class10 = School("Physics B", "Ms. Garcia", ["Gabriel", "Hannah", "Isaac"], 110, "Foreign Language")
 
 
 print(class1.class_name)
